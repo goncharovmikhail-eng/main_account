85a5dac59da8bea986ec4d4bcb5b50a3a533755f
gmomainsystem <gmomainsystem@gmail.com>
2024-11-02 18:26:18 +0300

    first commit

---
+++ alias.sh
@@ -0,0 +1,35 @@
+mreq(){
+  rm -rf ./roles
+  cat requirements.yml | grep -v ^# | grep https://gitlab.mcart.ru > /dev/null 2>&1
+  if [ $? -eq 1 ]; then
+    ansible-galaxy install -r requirements.yml -f -v
+  else
+    cat requirements.yml | sed 's/https:\/\/gitlab.mcart.ru\//git@gitlab.mcart.ru:/g' > /tmp/req.yml
+    ansible-galaxy install -r /tmp/req.yml -f -v
+    rm -f /tmp/req.yml
+  fi
+}
+alias req-dev='rm -rf ./roles ; ansible-galaxy install -r requirements-dev.yml -f -v'
+function ii() {
+        echo -e "\nВы находитесь на ${RED}$HOSTNAME ($(hostname -f))"
+        echo -e "\nДополнительная информация:$NC" ; uname -a
+        echo -e "\n${RED}В системе работают пользователи:$NC" ; w -h
+        echo -e "\n${RED}Дата:$NC" ; date '+%d.%m.%Y %H:%M:%S'
+        echo -e "\n${RED}Время, прошедшее с момента последней перезагрузки:$NC" ; uptime| sed 's/^[ \t]*//;s/[ \t]*$//'
+        echo -e "\n${RED}Память:$NC" ; free
+        GW=$(ip route | awk '/default/ { print $3 }')
+        echo -e "\n${RED}Основной шлюз:$NC" ; echo ${GW}
+}
+hg(){
+if [ ! -z "$3" ];then
+history | grep -i $1 | grep -i $2 | grep -i $3
+elif [ ! -z "$2" ];then
+history | grep -i $1 | grep -i $2
+else
+history | grep -i $1
+fi
+}
+alias ll='ls -lah'
+alias req="rm -rf ./roles ; ansible-galaxy install -r requirements.yml -f -v"
+alias pj="cd /home/m.goncharov/mcart/project/ && ls -lah"
+alias ro="cd /home/m.goncharov/mcart/roles/ && ls -lah"
---
+++ main_account.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if ! stat /home/goncharov/.zshrc.pre-oh-my-zsh &> /dev/null; then
+    echo "Zsh не найден, устанавливаем..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="3den"/' ~/.zshrc
+else
+    echo "Zsh уже установлен"
+fi
+read -p "Добавить SSH-ключи? (y/n): " add_keys
+
+if [[ "$add_keys" == "y" || "$add_keys" == "Y" ]]; then
+    chmod +x setup-ssh-keys.sh
+    ./setup-ssh-keys.sh
+fi
+if ! grep -Fxq "source alias.sh" ~/.bashrc; then
+    echo 'source alias.sh' >> ~/.bashrc
+    echo 'source alias.sh' >> ~/.zshrc
+fi
+if ! grep -Fxq "exec zsh" ~/.bashrc; then
+    echo 'exec zsh' >> ~/.bashrc
+fi
+echo "Настройка завершена. Перезапустите терминал, чтобы применить изменения."
