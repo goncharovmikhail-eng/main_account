aa7f7089b7a1800ced40fe374d1f66c57981915c
Goncharov <goncharov@craft.tech>
2025-06-20 17:00:55 +0300

    fix

--- fix.sh
+++ fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+mkdir -p ~/header_project/roles/main_role/files/
 ln -s ~/main_account/alias.sh ~/header_project/roles/main_role/files/alias.sh
+sudo cp d.sh /usr/local/bin/doc
+sudo chmod +x /usr/local/bin/doc
