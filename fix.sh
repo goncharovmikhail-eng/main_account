b3fdf5ede043cfc810a644160af17a8be86941ce
goncharovmikhail-eng <goncharov@PC037.crafttech.local>
2025-06-26 12:48:17 +0300

    fix

--- fix.sh
+++ fix.sh
@@ -3,3 +3,5 @@ mkdir -p ~/header_project/roles/main_role/files/
 ln -s ~/main_account/alias.sh ~/header_project/roles/main_role/files/alias.sh
 sudo cp d.sh /usr/local/bin/doc
 sudo chmod +x /usr/local/bin/doc
+sudo cp p.sh /usr/local/bin/pod
+sudo chmod +x /usr/local/bin/pod
