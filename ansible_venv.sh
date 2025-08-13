208b91292d4052e974c967536f2bb282f0302f77
goncharovmikhail-eng <goncharov@PC037.crafttech.local>
2025-08-13 17:06:15 +0300

    ansible_venv.sh

---
+++ ansible_venv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: ansible_venv.sh
+# 
+#         USAGE: ./ansible_venv.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Goncharov M. (), 
+#  ORGANIZATION: 
+#       CREATED: 08/13/2025 17:04
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+sudo apt update
+sudo apt install python3-venv -y
+python3 -m venv ~/ansible-2.9-env
+ansible --version
